efe1398b52b2cbfe285235b5faa1ef1cc425ac5e
vladimir pestov <pestov.web@yandex.ru>
2022-01-16 14:17:06 +1000

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+
+
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Сервер запущен на порту: ${PORT}`);
+});
